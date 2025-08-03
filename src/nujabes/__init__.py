55a509abf4a6072846bc0d279034c7e1959bf11e
LanceNoble <ltnlikespie@gmail.com>
2025-08-02 23:31:44 -0400

    Refactor

--- src/nujabes/__init__.py
+++ src/nujabes/__init__.py
@@ -6,8 +6,11 @@ from json import loads, dumps
 from random import random
 
 from websockets.asyncio.client import connect, ClientConnection
+from websockets.exceptions import ConnectionClosed
 
 from yt_dlp import YoutubeDL
+
+from constants import baseUrl, botToken, botHeaders
 
 async def beat(client: ClientConnection, connectionState: dict[str, int]):
     prevNumAcks = connectionState["numAcks"]
@@ -26,17 +29,11 @@ async def repeat(interval: int, client: ClientConnection, connectionState: dict[
         numBeats += 1
         await beat(client, connectionState)
 
-async def process(events: Queue, botToken: str):
+async def process(events: Queue):
     while True:
         event = await events.get()
-        response = get(
-            f"https://discord.com/api/v10/guilds/{event['d']['guild_id']}/voice-states/@me",
-            headers={
-                "User-Agent": "nujabes (https://github.com/LanceNoble/Nujabes, 1.0)",
-                "Authorization": f"Bot {botToken}",
-                "Content-Type": "application/json"
-            }
-        )
+        #if ()
+        response = get(f"{baseUrl}/guilds/{event['d']['guild_id']}/voice-states/@me", headers=botHeaders)
         voiceState = response.json()
         if response.status_code == 404:
             pass
@@ -44,68 +41,10 @@ async def process(events: Queue, botToken: str):
             pass
         events.task_done()
 
-async def respond(interactionId: str, interactionToken: str, botToken: str, guildId: str):
-    response = get(
-        f"https://discord.com/api/v10/guilds/{guildId}/voice-states/@me",
-        headers={
-            "User-Agent": "nujabes (https://github.com/LanceNoble/Nujabes, 1.0)",
-            "Authorization": f"Bot {botToken}",
-            "Content-Type": "application/json"
-        }
-    )
-    if response.status_code == 404:
-        create_task()
-    voiceState = loads(response.read())
-    
-    # response = urlopen(Request(
-    #     f"https://discord.com/api/v10/interactions/{interactionId}/{interactionToken}/callback",
-    #     dumps({
-    #         "type": 4,
-    #         "data": {
-    #             "content": "Congrats on sending your command!"
-    #         }
-    #     }).encode(),
-    #     {
-    #         "User-Agent": "nujabes (https://github.com/LanceNoble/Nujabes, 1.0)",
-    #         "Authorization": f"Bot {interactionToken}",
-    #         "Content-Type": "application/json"
-    #     }
-    # ))
-
 async def enter():
-    # ydl = YoutubeDL({ "format": "bestaudio", "skip_download": True })
-    # info = ydl.extract_info("https://www.youtube.com/watch?v=iqPAVCtRO3I")
-    # proc = await create_subprocess_exec(
-    #     "../../bin/ffmpeg.exe", "-i", info["url"], "-c:a" , "libopus", "-ar", "48000", "-ac", "2", "-f", "opus", "pipe:1",
-    #     stdout=PIPE
-    # )
-    # try:
-    #     while True:
-    #         chunk = await proc.stdout.read(4096)
-    #         if not chunk:
-    #             break
-    #         # Process the chunk (e.g., write to file, analyze, etc.)
-    #         print(f"Received {len(chunk)} bytes")
-    # finally:
-    #     await proc.wait()
-    # ydl.close()
-
-    stream = open("../../token")
-    botToken = stream.read()
-    stream.close()
-
-    response = get(
-        "https://discord.com/api/v10/gateway/bot",
-        headers={
-            "User-Agent": "nujabes (https://github.com/LanceNoble/Nujabes, 1.0)",
-            "Authorization": f"Bot {botToken}"
-        }
-    )
-    gate = response.json()
-    # b'{"url":"wss://gateway.discord.gg","session_start_limit":{"max_concurrency":1,"remaining":1000,"reset_after":0,"total":1000},"shards":1}\n'
-
+    gate = get(f"{baseUrl}/gateway/bot", headers=botHeaders).json()
     client = await connect(f"{gate['url']}/?v=10&encoding=json")
-    heartbeat = loads(await client.recv())
+    hello = loads(await client.recv())
 
     await client.send(dumps({
         "op": 2,
@@ -124,30 +63,44 @@ async def enter():
     if ready["op"] == 9:
         client.close()
         return
+    resumeUrl = ready["d"]["resume_gateway_url"]
 
     processors: list[Task] = []
     eventMap: dict[str, Queue] = {}
     for guild in ready["d"]["guilds"]:
         eventMap[guild["id"]] = Queue()
-        processors.append(create_task(process(eventMap[guild["id"]], botToken)))
+        processors.append(create_task(process(eventMap[guild["id"]])))
 
-    connectionState = {"numAcks": 0, "sequence": None}
-    interval = create_task(repeat(heartbeat["d"]["heartbeat_interval"], client, connectionState))
+    connectionState = {"numAcks": 0, "sequence": ready["s"]}
+    interval = create_task(repeat(hello["d"]["heartbeat_interval"], client, connectionState))
 
     while True:
-        event = loads(await client.recv())
-        print(event)
+        event
+        try:
+            event = loads(await client.recv())
+        except ConnectionClosed as e:
+            pass
         if event["op"] == 0:
-            # task = create_task(respond(event["d"]["id"], event["d"]["token"], token, event["d"]["guild_id"]))
-            # eventMap[event["d"]["guild_id"]].put(event)
+            eventMap[event["d"]["guild_id"]].put(event)
             connectionState["sequence"] = event["s"]
         elif event["op"] == 1:
             create_task(beat())
+        elif event["op"] == 7:
+            pass
         elif event["op"] == 11:
             connectionState["numAcks"] += 1
         
     interval.cancel()
-    for acoiewj in processors:
-        acoiewj.cancel()
+    for process in processors:
+        process.cancel()
 
-run(enter())
+async def foo():
+    while True:
+        await sleep(1)
+        print("foo")
+
+create_task(foo())
+while True:
+    pass
+
+#run(enter())
---
+++ src/nujabes/constants.py
@@ -0,0 +1,9 @@
+baseUrl = "https://discord.com/api/v10"
+_stream = open("../../token")
+botToken = _stream.read()
+_stream.close()
+botHeaders = {
+    "User-Agent": "nujabes (https://github.com/LanceNoble/nujabes, 1.0)",
+    "Authorization": f"Bot {botToken}",
+    "Content-Type": "application/json"
+}
